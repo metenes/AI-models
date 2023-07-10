86c8b567d8f1cd870dd4f0e99f3059e56806519a
Mete Enes Yılmaz <91368249+metenes@users.noreply.github.com>
2023-07-10 15:38:19 +0200

    Update simpleUnet.py

--- simpleUnet.py
+++ simpleUnet.py
@@ -1,8 +1,10 @@
+#%%
+
 import torch
 from torch import nn 
 
 class DoubleConv(nn.Module): 
-    def __init__(self, in_ch, out_ch, up): 
+    def __init__(self, in_ch, out_ch): 
         super(DoubleConv, self).__init__() # do not know why ? 
         self.Conv = nn.Sequential(
             # first layer
@@ -19,5 +21,23 @@ class DoubleConv(nn.Module):
         return self.Conv(x)
     
 class UNET(nn.Module): 
-    def __init__(self): 
+    def __init__(self, 
+                 in_ch = 3, 
+                 out_ch = 1, 
+                 features = [64,128,256,512] # Paper values
+                 ): 
+    
+        super(UNET, self).__init__() 
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size= 2, stride=2)
+
+        # Down conv. layers
+        self.downs.append(DoubleConv(in_ch, features[0] ))
+        for idx in range(1, len(features)-1): 
+            self.downs.append(DoubleConv(in_ch= features[idx], out_ch=features[idx+1]))
         
+        # Ups conv. layers
+        for idx in range(1, len(features)-1): 
+            self.ups.append(DoubleConv(in_ch= features[len(features) - idx], out_ch=features[len(features) - idx-1]))
+        self.ups.append(DoubleConv(in_ch= 0, out_ch= out_ch))
