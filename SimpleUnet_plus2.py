d36a6cf3a4d83880f9fb8a0ea83157b5d0e7d864
Mete Enes Yılmaz <91368249+metenes@users.noreply.github.com>
2023-07-24 16:27:29 +0200

    Update SimpleUnet_plus2.py

--- SimpleUnet_plus2.py
+++ SimpleUnet_plus2.py
@@ -1,6 +1,6 @@
 #%% 
 # Init models Black Box - Dont know what are these 
-# Probabily pre-defined weights and biases by trorch libary 
+# Probabily pre-defined weights and biases by torch libary 
 import torch
 import torch.nn as nn
 from torch.nn import init
@@ -99,6 +99,7 @@ class ConvBlock(nn.Module) :
                 in_size = out_size
 
         """ 
+        # May be in de-commented 
         # initialise the blocks
         for m in self.children():
             init_weights(m, init_type='kaiming')
@@ -123,6 +124,7 @@ class Up(nn.Module):
             self.up = nn.UpsamplingBilinear2d(scale_factor=2)
         
         """ 
+        # May be in de-commented 
         # initialise the blocks
         for m in self.children():
             if m.__class__.__name__.find('unetConv2') != -1: continue
@@ -135,9 +137,9 @@ class Up(nn.Module):
             outputs0 = torch.cat([outputs0, input[i]], 1)
         return self.conv(outputs0)
 
-class unetUp_origin(nn.Module):
+class mergeConv(nn.Module):
     def __init__(self, in_size, out_size, is_deconv, n_concat=2):
-        super(unetUp_origin, self).__init__()
+        super(mergeConv, self).__init__()
         # self.conv = unetConv2(out_size*2, out_size, False)
         if is_deconv:
             self.conv = ConvBlock(in_size + (n_concat - 2) * out_size, out_size, False)
@@ -147,6 +149,7 @@ class unetUp_origin(nn.Module):
             self.up = nn.UpsamplingBilinear2d(scale_factor=2)
 
         """ 
+        # May be in de-commented 
         # initialise the blocks
         for m in self.children():
             if m.__class__.__name__.find('unetConv2') != -1: continue
@@ -158,3 +161,108 @@ class unetUp_origin(nn.Module):
         for i in range(len(input)):
             outputs0 = torch.cat([outputs0, input[i]], 1)
         return self.conv(outputs0)
+#%% 
+# Unet+2 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torchvision import models
+
+class Unet_plus2(nn.Module): 
+    def __init__(self, 
+                 in_ch = 1, 
+                 out_ch = 1, 
+                 feature_scale = 4, 
+                 is_deconv = True,
+                 is_batchnorm = True,
+                 is_ds = True ): 
+        super(Unet_plus2, self).__init__()
+        # Init
+        self.is_deconv = is_deconv
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.is_batchnorm = is_batchnorm
+        self.is_ds = is_ds
+        self.feature_scale = feature_scale
+
+        # Filter channels
+        filters = [64, 128, 256, 512, 1024]
+
+        # Down Sample = Standart Unet
+        self.conv00 = ConvBlock(self.in_channels, filters[0], self.is_batchnorm)
+        self.maxpool0 = nn.MaxPool2d(kernel_size=2)
+        self.conv10 = ConvBlock(filters[0], filters[1], self.is_batchnorm)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv20 = ConvBlock(filters[1], filters[2], self.is_batchnorm)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        self.conv30 = ConvBlock(filters[2], filters[3], self.is_batchnorm)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        self.conv40 = ConvBlock(filters[3], filters[4], self.is_batchnorm)
+
+        # Up sampling 
+        self.up_concat01 = mergeConv(filters[1], filters[0], self.is_deconv)
+        self.up_concat11 = mergeConv(filters[2], filters[1], self.is_deconv)
+        self.up_concat21 = mergeConv(filters[3], filters[2], self.is_deconv)
+        self.up_concat31 = mergeConv(filters[4], filters[3], self.is_deconv)
+        self.up_concat02 = mergeConv(filters[1], filters[0], self.is_deconv, 3)
+        self.up_concat12 = mergeConv(filters[2], filters[1], self.is_deconv, 3)
+        self.up_concat22 = mergeConv(filters[3], filters[2], self.is_deconv, 3)
+        self.up_concat03 = mergeConv(filters[1], filters[0], self.is_deconv, 4)
+        self.up_concat13 = mergeConv(filters[2], filters[1], self.is_deconv, 4)
+        self.up_concat04 = mergeConv(filters[1], filters[0], self.is_deconv, 5)
+
+        # Final Convulution 
+        self.final_1 = nn.Conv2d(filters[0], out_ch, 1)
+        self.final_2 = nn.Conv2d(filters[0], out_ch, 1)
+        self.final_3 = nn.Conv2d(filters[0], out_ch, 1)
+        self.final_4 = nn.Conv2d(filters[0], out_ch, 1)
+
+    def forward(self, inputs):
+    
+        """
+            In naming section: 
+                X_row_colm
+                X_01 = row 0 and col 1 
+        """
+
+        # column : 0        
+        X_00 = self.conv00(inputs)
+        maxpool0 = self.maxpool0(X_00)
+        X_10 = self.conv10(maxpool0)
+        maxpool1 = self.maxpool1(X_10)
+        X_20 = self.conv20(maxpool1)
+        maxpool2 = self.maxpool2(X_20)
+        X_30 = self.conv30(maxpool2)
+        maxpool3 = self.maxpool3(X_30)
+        X_40 = self.conv40(maxpool3)
+        # column : 1
+        X_01 = self.up_concat01(X_10, X_00)
+        X_11 = self.up_concat11(X_20, X_10)
+        X_21 = self.up_concat21(X_30, X_20)
+        X_31 = self.up_concat31(X_40, X_30)
+        # column : 2
+        X_02 = self.up_concat02(X_11, X_00, X_01)
+        X_12 = self.up_concat12(X_21, X_10, X_11)
+        X_22 = self.up_concat22(X_31, X_20, X_21)
+        # column : 3
+        X_03 = self.up_concat03(X_12, X_00, X_01, X_02)
+        X_13 = self.up_concat13(X_22, X_10, X_11, X_12)
+        # column : 4
+        X_04 = self.up_concat04(X_13, X_00, X_01, X_02, X_03)
+
+        # Final layer
+        final_1 = self.final_1(X_01)
+        final_2 = self.final_2(X_02)
+        final_3 = self.final_3(X_03)
+        final_4 = self.final_4(X_04)
+
+        final = (final_1 + final_2 + final_3 + final_4) / 4
+
+        if self.is_ds:
+            return F.sigmoid(final)
+        else:
+            return F.sigmoid(final_4)
+        
+# Models output 
+model = Unet_plus2()
